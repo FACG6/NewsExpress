37c66e5b5f24c731868c5fcb994160839b5a41c3
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-26 14:40:15 +0200

    add test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 const tape=require('tape');
+const runDbBuild=require('../src/dataBase/db_build');
 tape('testing the test',(t)=>{
+    runDbBuild
     t.equal(1,1,"should return one");
     t.end();
 })
