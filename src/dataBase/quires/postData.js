b2216f866efb81a8e82ef10feb93224b560bbc36
Khader <kakastarcris@gmail.com>
2019-02-26 16:05:06 +0200

    check if there is a cookie or not Relates #16

--- src/dataBase/quires/postData.js
+++ src/dataBase/quires/postData.js
@@ -1,12 +1,11 @@
 const connect = require('../db_connection');
-const {
-    hashpassword
-} = require('../commands/hashpassword.js');
+const { hashpassword } = require('../commands/hashpassword.js');
 
-const addauthor = (name , email ,password,cb) =>{
-    const hasedpassword = hashpassword(password);
-    const querycommand = 'INSET INTO journalist (fullname,email,password) Values ($1, $2, $3)';
-    const values = [name, email, hashedPassword];
-    connect.query(text, values, callback);
-}
+const addauthor = (name, email, password, cb) => {
+  const hasedpassword = hashpassword(password);
+  const querycommand = 'INSET INTO journalist (fullname,email,password) Values ($1, $2, $3)';
+  const values = [name, email, hasedpassword];
+  connect.query(querycommand, values, cb);
+};
+
 module.exports = addauthor;
