fe9b343fee140c3fe9fafd3cba16a851958e08d2
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-27 11:08:55 +0200

    pull master

--- src/controllers/checkUser.js
+++ src/controllers/checkUser.js
@@ -1,49 +1,49 @@
-// const bcrypt = require('bcrypt');
-// const checkUser = require('./../dataBase/quires/checkData');
-// require('dotenv').config();
-// const {
-//   sign,
-// } = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const checkUser = require('./../dataBase/quires/checkData');
+require('dotenv').config();
+const {
+  sign,
+} = require('jsonwebtoken');
 
-// const compare = (password, hashedPassword) => new Promise((resolve, reject) => {
-//   bcrypt.compare(password, hashedPassword, (err, success) => {
-//     if (err) {
-//       reject(err);
-//     } else {
-//       resolve(success);
-//     }
-//   });
-// });
+const compare = (password, hashedPassword) => new Promise((resolve, reject) => {
+  bcrypt.compare(password, hashedPassword, (err, success) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(success);
+    }
+  });
+});
 
-// exports.checkUser = (req, res) => {
-//   const { emailValue, passwordValue } = req.body;
-//   checkUser(emailValue).then((rows) => {
-//     if (rows.rows[0]) {
-//       compare(passwordValue, rows.rows[0].password).then((result) => {
-//         if (result) {
-//           const payload = {
-//             id: rows.rows[0].id,
-//             name: rows.rows[0].name,
-//           };
-//           const jwt = sign(payload, process.env.SECRET);
-//           res.writeHead({
-//             'Set-Cookie': [`jwt=${jwt};httpOnly;MaxAge=10000`],
-//           });
-//           res.send({
-//             successMessage: true,
-//           });
-//         } else {
-//           res.send(JSON.stringify({
-//             successMessage: 'Password is invalid',
-//           }));
-//         }
-//       });
-//     } else {
-//       res.send(JSON.stringify({
-//         successMessage: 'Email is invalid',
-//       }));
-//     }
-//   }).catch((err) => {
-//     console.log(err);
-//   });
-// };
+exports.checkUser = (req, res) => {
+  const { emailValue, passwordValue } = req.body;
+  checkUser(emailValue).then((rows) => {
+    if (rows.rows[0]) {
+      compare(passwordValue, rows.rows[0].password).then((result) => {
+        if (result) {
+          const payload = {
+            id: rows.rows[0].id,
+            name: rows.rows[0].name,
+          };
+          const jwt = sign(payload, process.env.SECRET);
+          res.writeHead({
+            'Set-Cookie': [`jwt=${jwt};httpOnly;MaxAge=10000`],
+          });
+          res.send({
+            successMessage: true,
+          });
+        } else {
+          res.send(JSON.stringify({
+            successMessage: 'Password is invalid',
+          }));
+        }
+      });
+    } else {
+      res.send(JSON.stringify({
+        successMessage: 'Email is invalid',
+      }));
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+};
