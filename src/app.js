bc53837435a142abcfedc393f01f2676528ec627
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-27 09:58:40 +0200

    add home render

--- src/app.js
+++ src/app.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-// const controllers = require('./controllers');
-// const helpers = require("./views/helpers/index");
-
-
-// const favicon = require('serve-favicon');
+// const favicon = require("serve-favicon");
 const bodyparser = require('body-parser');
-// const routes = require('./routes');
+const router = require('./routers/index');
+const helpers = require('./views/helpers/index');
 
 const app = express();
 
 app.disable('x-powerd-by');
 app.set('port', process.env.PORT || 3000);
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: false}));
-// app.use(controllers);
-app.set('view', path.join(__dirname, 'views'));
-app.set('view-engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.engine(
     'hbs',
     exphbs({
@@ -25,10 +19,12 @@ app.engine(
         layoutsDir: path.join(__dirname, 'views', 'layouts'),
         partialsDir: path.join(__dirname, 'views', 'partials'),
         defaultLayout: 'main',
-        // helpers,
-    })
-);
-// app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+    }),
+    );
+    // app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
+    // app.use(express.static(path.join(__dirname, "..", "public")));
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({ extended: false }));
+    app.use(router);
 
 module.exports = app;
